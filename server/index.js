8e17a86bd19ce10179c9824b493871964f4e449b
thirumurugan7 <thiru82003@gmail.com>
2023-01-27 00:35:07 +0530

    configuration over

--- server/index.js
+++ server/index.js
@@ -5,3 +5,16 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+// Configurations
+
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); //for cross-origin requests
+app.use(morgan("common")); //for api calls from different server
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
