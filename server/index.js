dfd1897081bba073236c28e055ec78a2bef2e255
thirumurugan7 <thiru82003@gmail.com>
2023-01-27 00:29:15 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
